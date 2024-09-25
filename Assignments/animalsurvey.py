450c90b8ebd3590be09c574f59070c21951d5b61
OliviaNelson15 <821396@mystma.org>
2024-09-25 09:42:27 -0500

    Animal Survey with comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,18 +1,18 @@
 
-user_input = input ("What is your favorite animal?")
+user_input = input ("What is your favorite animal?")     #This survey asks about your favoirte animal and then prints a summary of your responses.
 print("You entered: " + user_input)
 
 
-user_input2 = input ("Why do you like them?")
+user_input2 = input ("Why do you like them?")    #Ask why you like them
 print("You entered: " + user_input2)
 
 
-user_input3 = input ("What do they eat?")
+user_input3 = input ("What do they eat?")      #Ask what they eat
 print("You entered: " + user_input3)
 
 
-user_input4 = input ("What is a fun fact about them?")
+user_input4 = input ("What is a fun fact about them?")     #Ask for a fun fact about the animal
 print("You entered: " + user_input4)
 
 
-print("Your favorite animal is user_input because:" user_input + user_input2 + user_input3 + user_input4)
+print("Your favorite animal is user_input because:" + user_input + user_input2 + user_input3 + user_input4)   #This is a summary of your responses.
