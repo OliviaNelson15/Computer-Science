6c3c0c9970a4b20c97e3d2666b442e123e8c2008
OliviaNelson15 <821396@mystma.org>
2024-09-25 09:02:58 -0500

    Sept 25th

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,17 +1,18 @@
-print("What is your favorite animal?")
-user_input = input ("A fox")
+
+user_input = input ("What is your favorite animal?")
 print("You entered: " + user_input)
 
-print("Why do you like them?")
-user_input2 = ("Because they are small, cute, and unique")
+
+user_input2 = input ("Why do you like them?")
 print("You entered: " + user_input2)
 
-print("What do they eat?")
-user_input3 = input ("Foxes are omnivores, so they usually eat small rodents and berries")
+
+user_input3 = input ("What do they eat?")
 print("You entered: " + user_input3)
 
-print("What is a fun fact about them?")
-user_input4 = input ("Unlike other canines, some foxes have semi-retractable claws, depending on the type of fox.")
+
+user_input4 = input ("What is a fun fact about them?")
 print("You entered: " + user_input4)
 
-print("Your favorite animal is a fox because they are cute, unique, and interesting creatures, and they have many interesting differences from other types of canines. They feed on small rodents and have retractable claws.")
+
+print(user_input + user_input2 + user_input3 + user_input4)
