702949b8dfab1e2ecea54b2064bd50a94cd13212
OliviaNelson15 <821396@mystma.org>
2024-09-27 09:42:23 -0500

    Final save Sept 27th

--- Unit 2 - Functions and Variables/Practice Sept. 27th.py
+++ Unit 2 - Functions and Variables/Practice Sept. 27th.py
@@ -1,14 +1,35 @@
 def add(x, y):
-    print(x + y):
+    print(x + y)
 
 num_1 = int(input("What's the first number?"))
 num_2 = int(input("What's the second number?"))
 
 add(num_1, num_2)
 
-def top_five_movies():
-    print(Young Woman and the Sea)
-    print(Mulan (The Animated Version))
-    print(Skyscraper)
-    (Bolt)
-    (Puss In Boots: The Last Wish)
+def top_five_movies(Movie_1, Movie_2, Movie_3, Movie_5):
+    print("Olivia's top 5 movies")
+    print(Movie_1)
+
+
+x = 4
+def my_function():
+    global x  #From now on, when I call x, I'm referring to the GLOBAL version, NOT the local version.
+    x = 5     #Reassigning the global variable x
+    print(x)  #Prints 5
+def my_function():
+        Y = 6
+        print(x + y)
+
+#It's 100% okay for functions not to return anything.
+
+calculate_tax(Item + float + rate)
+item = ("A million golden raspberries")
+float = (10,000)
+rate = (6.875)
+
+
+
+
+
+
+
