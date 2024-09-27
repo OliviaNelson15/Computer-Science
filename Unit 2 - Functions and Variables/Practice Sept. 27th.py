62199ffc07fc289608f11cfd85d5b2dd93477384
OliviaNelson15 <821396@mystma.org>
2024-09-27 09:05:39 -0500

    Another save on Sept. 27th.py

---
+++ Unit 2 - Functions and Variables/Practice Sept. 27th.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    print(x + y):
+
+num_1 = int(input("What's the first number?"))
+num_2 = int(input("What's the second number?"))
+
+add(num_1, num_2)
+
+def top_five_movies():
+    print(Young Woman and the Sea)
+    print(Mulan (The Animated Version))
+    print(Skyscraper)
+    (Bolt)
+    (Puss In Boots: The Last Wish)
