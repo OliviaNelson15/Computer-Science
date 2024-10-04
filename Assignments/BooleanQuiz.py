644f1d58fb9f6f397c0d54b97fa35bd10ace86d6
OliviaNelson15 <821396@mystma.org>
2024-10-04 09:27:53 -0500

    BooleanQuiz.py

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,14 @@
+answer = int(input("Give a whole number that is less than 10.\n"))
+print(answer < 10)
+
+answer = int(input("Give a whole number that is greater than or equal to 3.\n"))
+print(answer >= 3)
+
+answer = int(input("Give a whole number that is greater than 5.\n"))
+print(answer > 5)
+
+answer = int(input("Give a whole number that is less than or equal to 9.\n"))
+print(answer <= 9)
+
+answer =int(input("Give a whole number that is greater than 8.\n"))
+print(answer > 8)
---
+++ Unit 2 - Functions and Variables/Practice Oct. 4th.py
@@ -0,0 +1,17 @@
+print(5 > 10)
+# values: 5, 10
+# realtional operator: >
+
+
+x = "blue"
+print(x == "yellow")
+# values: x, "yellow"
+# realtional operator: ==
+
+
+answer = "cube"
+count = 1
+print(answer == "cube" and count < 10)
+# values: answer, "cube", count, 10
+# realtional operators: ==, <
+# logical operators: and
