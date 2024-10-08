62225d92f70c7592f43fa3d9e59fccf9697733d1
OliviaNelson15 <821396@mystma.org>
2024-10-08 09:48:38 -0500

    Oct 8th

---
+++ Assignments/SimpleQuiz.py
@@ -0,0 +1,23 @@
+#A VERY EASY QUIZ THAT WILL DETERMINE THE REST OF YOUR FUTURE!!! :O
+
+a1=input("Type the number 7.\n")
+a2 =input("What is 8 - 1?\n")
+a3 = input("Do worms have eyes?\n")
+a4= input("Type the number 4.\n")
+a5= input("What is 2 + 2? \n")
+
+def tally_score():
+    score = 0
+    if a1 == "7":
+        score = score + 1
+    if a2 == "7":
+        score = score + 1
+    if a3 == "no":
+        score == score + 1
+    if a4 == "4":
+        score == score + 1
+    if a5 == "4":
+        score == score + 1
+
+print(score)
+
---
+++ Unit 3 - Conditionals and Exceptions/stringfunctions.py
@@ -0,0 +1,31 @@
+#String functions
+#A group of like-functions that manipulate atrings
+#They modify strings
+#SUPER easy + convenient to use
+#Python would suck without them
+
+#     .Lower()
+#converts string to all lowercase
+#no matter what the input casing is, ti is converted to lowercase
+#and the answer is lowercase
+input_answer = "Lord of The Rings"
+real_answer = "Lord of The Rings"
+print(input_answer == real_answer)
+
+# .upper()
+# Converts a string to uppercase!!! Wooooowwww
+x = "ur mom" .upper ()
+
+# .capitalize()
+#converts to lowercase, then capitalizes the first letter
+y = "uR mOm:" .capitalize()
+print(y)
+
+# .title()
+#Converts a string to titlecase
+
+# .swapcase()
+#Inverts the casing of each character
+q = "Hello World!" .swapcase()
+print(q) #prints "hELLO wORLD!"
+
