3f7e873636eb482cc0de8ddd42a76231e82d0752
OliviaNelson15 <821396@mystma.org>
2024-10-15 09:34:56 -0500

    Oct 15th part 2

---
+++ Unit 3 - Conditionals and Exceptions/Freecoding.py
@@ -0,0 +1,8 @@
+#Calculaator lol
+
+def add_two_numbers():
+        x=(int("What is the first numbber?\n"))
+        y=(int("What is the second number?\n"))
+
+        print(x + y)
+        add_two_numbers()
