6e1f0331826f9b2254f058ba0b398a3f6830f87b
OliviaNelson15 <821396@mystma.org>
2024-10-31 09:48:09 -0500

    Saved work oct. 31st

---
+++ Unit 4 - Loops/Oct 31st.py
@@ -0,0 +1,34 @@
+#Python has 4 types of collections
+#Tuple
+#Set
+#list
+#Dictionary
+
+#Today we will focus on listsssssssssss
+#A list is a way to store more that one value in a variable
+#Those values  in the list are called "items"
+#ITEMS can be accessed by their index, which is there position in the list.
+#INDEX                      0                       1               2               3
+
+best_elden_ring_weapons = ["Blashphemous Blade", "Moonveil", "Rivers of Blood", "Iron Ball"]
+#INDEX          0   1       2   3
+best_years = [1776, 1985, 1993,1957]
+
+#Print the best elden ring weapon
+print(best_elden_ring_weapons[0])
+
+print(len(best_elden_ring_weapons))
+
+#Add new item to the end of a list
+best_elden_ring_weapons.append
+import random
+numbers = [random.randint(1,100), random.randint(1,100), random.randint92,100]
+print(numbers)
+print(max(numbers))
+print(min(numbers))
+
+#Strings are lists! They're just a list of characters.
+#List items can be changed.
+
+best_elden_ring_weapons.pop()
+#the .pop() function removes an item on a list by its position on the list
