035e57c06ae000e4d7e9af225b9fc57feb693266
OliviaNelson15 <821396@mystma.org>
2024-11-04 09:54:06 -0600

    Saved changes nov 4th

---
+++ Unit 4 - Loops/manualsorting.py
@@ -0,0 +1,38 @@
+#Start with bubble sort
+#Apparently it should be around 13 lines...
+#Generate a list of numbers from 0-99, and print it before it's sorted
+
+numbers = [34, 7, 23, 32, 5, 62]
+
+def bubblesort(numbers):   #Take list as a parameter.
+    #create a variable for the amount of steps we are taking. Start at 0
+    steps = 0
+    #Repeatedly check if list is sorted as long as there are list items
+    for i in range(0, len(numbers) - 1):
+        for j in range(0,len(nums)-1): #Iterate for every item in list
+            if numbers[1] > numbers[i +1]:
+                #Check if current list item is greater than next.
+                numbers[i], numbers [i +1] = numbers{i+1}
+            numbers[1] = numbers[i+1] #Swap values if needed.
+            numbers[i+1] = a
+
+            #Increase number of steps somewhere down here
+            
+
+        steps=0
+        bubblesortsteps:
+
+#...I'll just work on merge sort for now.
+
+def mergesort(numbers):
+    num1 = 34
+    num2=7
+    num3=23
+    num4=32
+    num5=5 #Lol
+    num6=62
+
+    for num in numbers:
+        print(numbers)
+        if num1 > num2:
+            
