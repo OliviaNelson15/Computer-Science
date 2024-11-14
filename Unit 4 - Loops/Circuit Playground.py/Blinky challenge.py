c8ef79b70dda69631d386304b1d7428ce5d4635b
OliviaNelson15 <821396@mystma.org>
2024-11-14 09:46:30 -0600

    hehe

--- Unit 4 - Loops/Circuit Playground.py/Blinky challenge.py
+++ Unit 4 - Loops/Circuit Playground.py/Blinky challenge.py
@@ -1,4 +1,3 @@
-
 #Drawer # 7
 
 #Imports tools needed to interact with the board
@@ -32,72 +31,9 @@
 import time
 from adafruit_circuitplayground import cp
 
-#Blinky
-#On, off, on, off
-while True:        #He said write an "if" statement??
-    cp.pixels[0]=(0,255,0)
-    cp.pixels[1]=(0,255,0)
-    cp.pixels[2]=(0,255,0)
-    cp.pixels[3]=(0,255,0)
-    cp.pixels[4]=(0,255,0)
-    cp.pixels[5]=(0,255,0)
-    cp.pixels[6]=(0,255,0)
-    cp.pixels[7]=(0,255,0)
-    cp.pixels[8]=(0,255,0)
-    cp.pixels[9]=(0,255,0)
-    cp.pixels[10]=(0,255,0)
-    cp.pixels[11]=(0,255,0)
-    cp.pixels[12]=(0,255,0)
-    cp.pixels[13]=(0,255,0)
-
-    cp.pixels.fil((0, 255, 0))
-    time.color((0,255,0))
-    time.sleep(367)
-    if cp.pixels.fil((0, 255, 0)) == True:
-        break
 
 while True:
-    cp.pixels[0]=(0.0)
-    cp.pixels[1]=(0.0)
-    cp.pixels[2]=(0.0)
-    cp.pixels[3]=(0.0)
-    cp.pixels[4]=(0.0)
-    cp.pixels[5]=(0.0)
-    cp.pixels[6]=(0.0)
-    cp.pixels[7]=(0.0)
-    cp.pixels[8]=(0.0)
-    cp.pixels[9]=(0.0)
-    cp.pixels[10]=(0.0)
-    cp.pixels[11]=(0.0)
-    cp.pixels[12]=(0.0)
-    cp.pixels[13]=(0.0)
-
-    cp.pixels.fil((0, 255, 0))
-    time.color((0,255,0))
-    time.sleep(367)
-    if cp.pixels.fil((0, 255, 0)) == True:
-        break
-
-#Come back to this one.
-
-#Woosh
-
-if cp.button_a:
-    print("Button A is pressed!")
-    while True:
-        if cp.button_a:
-             cp.pixels.fill((0, 0, 255))
-             while cp.button_a:
-                 pass
-        else:
-            cp.pixels.fill((0,0,0))
-
-mode = 0 
-
-while True:
-    if cp.button_a:
-        mode = (mode + 1) % 3 
-        print("Mode:", mode)
-        while cp.button_a:
-            pass
-
+    cp.pixels.fill((0,255,0))
+    time.sleep((0,367))
+    cp.pixels.fill((0,0,0))
+    time.sleep((0,367))
