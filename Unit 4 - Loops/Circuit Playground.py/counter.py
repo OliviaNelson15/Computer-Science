40ba77b68567c6a451cf84d4f777fa49a52b5491
OliviaNelson15 <821396@mystma.org>
2024-11-21 09:36:53 -0600

    lol

---
+++ Unit 4 - Loops/Circuit Playground.py/counter.py
@@ -0,0 +1,38 @@
+from adafruit_circuitplayground import cp
+#import time
+
+#cp.pixels.brightness = 0.3
+
+#while True:
+        #cp.pixels.fill((0,255,0))
+        #time.sleep(0.367)
+        #cp.pixels.fill((0,0,0))
+        #time.sleep(0.367)
+
+
+#Counter
+
+while True:
+    cp.pixels.fill((0,0,0))
+    if cp.button_a:
+        cp.pixels[0]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[1]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[2]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[3]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[4]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[5]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[6]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[7]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[8]=((70, 7, 82))
+    if cp.button_a:
+        cp.pixels[9]=((70, 7, 82))
+
+        #Currently unfinished
--- Unit 4 - Loops/Circuit Playground.py/thermometer.py
+++ Unit 4 - Loops/Circuit Playground.py/thermometer.py
@@ -35,5 +35,26 @@ while True:
         cp.pixels[8] = ((1,0,0))
     if temp_f > 86:
         cp.pixels[9] = ((1,0,0))
-
-    #NOT DONE YET
+    else:
+        temp_c = cp.temperature
+        temp_f = (temp_c * 9 / 5) + 32
+        if temp_f < 78:
+            cp.pixels[0] = ((0,0,0))
+        if temp_f > 78:
+            cp.pixels[1] = ((0,0,0))
+        if temp_f > 79:
+            cp.pixels[2] = ((0,0,0))
+        if temp_f > 80:
+            cp.pixels[3] = ((0,0,0))
+        if temp_f > 81:
+            cp.pixels[4] = ((0,0,0))
+        if temp_f > 82:
+            cp.pixels[5] = ((0,0,0))
+        if temp_f > 83:
+            cp.pixels[6] = ((0,0,0))
+        if temp_f > 84:
+            cp.pixels[7] = ((0,0,0))
+        if temp_f > 85:
+            cp.pixels[8] = ((0,0,0))
+        if temp_f > 86:
+            cp.pixels[9] = ((0,0,0))
